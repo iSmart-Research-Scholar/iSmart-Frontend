c579be9866c43cab3d065c584f7a6f256fad0452
Vijay Karanjkar <vijaykaranjkar2003@gmail.com>
2022-10-16 06:00:38 +0530

    Summarizer Added

--- src/components/DocumentForm.js
+++ src/components/DocumentForm.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState } from "react";
+import { PickerOverlay } from "filestack-react";
+import axios from "axios";
 
 function DocumentForm() {
-    return <>Document Form</>;
+    const [summary, setSummary] = useState("");
+    // const [url, setUrl] = useState("");
+    const [isUploaded, setIsUploaded] = useState(false);
+    console.log(isUploaded);
+    async function getSummary(url) {
+        
+        console.log("In Summary", "https://isummarizer.herokuapp.com/summarize/?url="+url);
+        // if(url!=="")
+        await axios({
+            method: "GET",
+            url: `https://isummarizer.herokuapp.com/summarize/?url=${url}`,
+        })
+            .then((data) => {
+                console.log(data.data.summary);
+                setSummary(data.data.summary);
+            })
+            .catch((err) => console.log(err));
+    }
+
+    return (
+        <div>
+            {isUploaded ? null : (
+                <PickerOverlay
+                    apikey={"AO72ws0hXQwinkoUdOPDOz"}
+                    onUploadDone={(res) => {
+                        console.log(res.filesUploaded[0].url);
+                        // setUrl(res.filesUploaded[0].url);
+                        setIsUploaded(true);
+                        getSummary(res.filesUploaded[0].url);
+                    }}
+                />
+            )}
+            <h4>Summary :</h4>
+            {summary ? summary : null}
+        </div>
+    );
 }
 
 export default DocumentForm;
